cd0479339a3f2097d81c4e3cf038d0095f2e5e0e
mikechess <mikecook450@gmail.com>
2024-02-02 15:13:46 -0500

    03. Button for component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,42 +1,50 @@
 import React, { useEffect, useState } from 'react'
 
 interface QuoteTypes {
-    quote: string;
-    author: string;
+  quote: string
+  author: string
 }
 
 const Card = () => {
-    const [randQuotes, setRandQuotes] = useState<QuoteTypes[]>([])
-    const [loading, setLoading] = useState(false)
+  const [randQuotes, setRandQuotes] = useState<QuoteTypes[]>([])
+  const [loading, setLoading] = useState(false)
 
-    useEffect(() => {
-        fetchQuotes()
-    }, [])
+  useEffect(() => {
+    fetchQuotes()
+  }, [])
 
-    const fetchQuotes = async () => {
-        try {
-            const response = await fetch("https://api.breakingbadquotes.xyz/v1/quotes")
-            const quotes = await response.json()
-            setRandQuotes(quotes)
-            setLoading(true)
-        } catch {
-            console.error("Something Went Wrong")
-        }
+  const fetchQuotes = async () => {
+    try {
+      const response = await fetch(
+        'https://api.breakingbadquotes.xyz/v1/quotes'
+      )
+      const quotes = await response.json()
+      setRandQuotes(quotes)
+      setLoading(true)
+    } catch {
+      console.error('Something Went Wrong')
     }
+  }
 
   return (
-    <div className='container'>
+    <>
+      <div className="container">
         {!loading ? (
-            <div className='loading-spinner'>Loading</div>
-        ) : <h2>Breaking Bad Quotes</h2>}
+          <div className="loading-spinner">Loading</div>
+        ) : (
+          <h2>Breaking Bad Quotes</h2>
+        )}
 
         {randQuotes.map((quote, index) => (
-            <span key={index}>
-                <p>{quote.quote}</p>
-                <h4>- {quote.author}</h4>
-            </span>
+          <span key={index}>
+            <p>{quote.quote}</p>
+            <h4>- {quote.author}</h4>
+          </span>
         ))}
-    </div>
+      </div>
+
+      <button onClick={() => window.location.reload()}>Next</button>
+    </>
   )
 }
 
